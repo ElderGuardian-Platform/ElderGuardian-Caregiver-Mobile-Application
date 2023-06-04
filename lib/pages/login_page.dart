de453bd4e56c0bc65e39825104ad564a2ebbfba0
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-06-04 10:36:36 +0530

    removed username textfield: redundant functionality

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../components/my_button.dart';
 import '../components/my_textfield.dart';
-import '../components/square_tile.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -43,45 +42,47 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset(
-                'lib/icons/ElderGuardian.png',
-                height: 300,
+                'lib/icons/ElderGuardian2.png',
+                height: 150,
               ),
             ],
           ),
         ),
 
-        //const SizedBox(height: 20),
+        const SizedBox(height: 20),
 
         //Welcome back username text widget
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-          child: const Column(
+        const Padding(
+          padding: EdgeInsets.fromLTRB(90, 0, 10, 10),
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                "Welcome back,",
+                "Welcome back, User Name",
                 style: TextStyle(
-                    fontSize: 30,
+                    fontSize: 20,
                     fontWeight: FontWeight.bold,
                     color: Colors.white),
               ),
-              Text(
-                "User Name",
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
+              // Text(
+              //   "User Name",
+              //   style: TextStyle(
+              //       fontSize: 20,
+              //       fontWeight: FontWeight.bold,
+              //       color: Colors.white),
+              // ),
             ],
           ),
         ),
 
-        // username textfield
-        MyTextField(
-          controller: usernameController,
-          hintText: 'Username',
-          obscureText: false,
-        ),
+        const SizedBox(height: 10),
+
+        //username textfield
+        //MyTextField(
+        //controller: usernameController,
+        //hintText: 'Username',
+        //   obscureText: false,
+        // ),
 
         const SizedBox(height: 10),
         // password textfield
@@ -90,7 +91,7 @@ class _LoginPageState extends State<LoginPage> {
           hintText: 'Password',
           obscureText: true,
         ),
-        const SizedBox(height: 10),
+        const SizedBox(height: 30),
         //sign in button
         MyButton(
           onTap: signUserIn,
