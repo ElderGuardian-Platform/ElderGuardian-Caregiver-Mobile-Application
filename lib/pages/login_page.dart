0d7f3cbf3304a29b2a9cf0dd6fe9c0a90511886d
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-06-17 13:05:59 +0530

    inserted background for login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../components/my_button.dart';
-import '../components/my_textfield.dart';
+//import '../components/my_button.dart';
+//import '../components/my_textfield.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -31,72 +31,15 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        //custom app bar
-        Padding(
-          padding: EdgeInsets.fromLTRB(10, 10, 0, 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'lib/icons/ElderGuardian2.png',
-                height: 150,
-              ),
-            ],
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'lib/icons/caregiver_elder_final.png',
+            ),
           ),
-        ),
-
-        const SizedBox(height: 20),
-
-        //Welcome back username text widget
-        const Padding(
-          padding: EdgeInsets.fromLTRB(90, 0, 10, 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Welcome back, User Name",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-              // Text(
-              //   "User Name",
-              //   style: TextStyle(
-              //       fontSize: 20,
-              //       fontWeight: FontWeight.bold,
-              //       color: Colors.white),
-              // ),
-            ],
-          ),
-        ),
-
-        const SizedBox(height: 10),
-
-        //username textfield
-        //MyTextField(
-        //controller: usernameController,
-        //hintText: 'Username',
-        //   obscureText: false,
-        // ),
-
-        const SizedBox(height: 70),
-        // password textfield
-        MyTextField(
-          controller: passwordController,
-          hintText: 'Password',
-          obscureText: true,
-        ),
-        const SizedBox(height: 30),
-        //sign in button
-        MyButton(
-          onTap: signUserIn,
-        ),
-      ])),
+        ],
+      ),
     );
   }
 }
